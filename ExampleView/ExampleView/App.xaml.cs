b021aa72aeb6ed1bca825d15bd7e78d6fae48ac1
Emna Dammak <e.dammak@root.sa>
2018-03-05 11:08:48 +0100

    Add project files.

---
+++ ExampleView/ExampleView/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ExampleView
+{
+	public partial class App : Application
+	{
+	    public App()
+	    {
+	        InitializeComponent();
+
+	        MainPage = new NavigationPage(new MenuPage1())
+            {
+	            BarBackgroundColor = Color.FromHex("#dcb34a")
+	        };
+            
+            
+        }
+
+	    protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ ExampleView/ExampleView/MenuPage1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExampleView
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuPage1 : MasterDetailPage
+    {
+        public MenuPage1()
+        {
+            InitializeComponent();
+
+            ListView1.ItemsSource = new string[]
+            {
+                "My ACCOUNT",
+                "Shop",
+                "FAQ",
+                "NOTIFICATIONS"
+
+            };
+           
+        }
+
+
+        private void ListView1_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+           if (ListView1.SelectedItem.Equals("My ACCOUNT"))
+            {
+                Detail=new MyAccount();
+                IsPresented = false;
+            }
+            else 
+               if (ListView1.SelectedItem.Equals("Shop"))
+               {
+                   Detail = new ShopPage();
+                   IsPresented = false;
+               }
+               else
+               if (ListView1.SelectedItem.Equals("FAQ"))
+               {
+                   Detail = new FAQPage();
+                   IsPresented = false;
+               }
+               else
+               if (ListView1.SelectedItem.Equals("NOTIFICATIONS"))
+               {
+                   Detail = new NotificationsPage();
+                   IsPresented = false;
+               }
+        }
+    }
+}
